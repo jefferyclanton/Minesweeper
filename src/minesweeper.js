466fe3f8173c231d367e05ef43f08034363c1839
jefferyclanton <37197975+jefferyclanton@users.noreply.github.com>
2018-03-29 20:49:18 -0400

    Fixed if statement in playMove function

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -4,7 +4,7 @@ class Game {
   }
   playMove(rowIndex, columnIndex) {
     this._board.flipTile(rowIndex, columnIndex);
-    if(this._board.playerBoard[rowIndex, columnIndex] === 'B' ) {
+    if(this._board.playerBoard[rowIndex][columnIndex] === 'B' ) {
       console.log('Bummer! Game Over!');
       this._board.print();
     } else if(!this._board.hasSafeTiles()) {
